21eddef78f5fb7de774894dcff55eeb6ef240c29
Leo Zhang <yzhang252@dxc.com>
2024-07-19 14:23:27 +0800

    Ensure minimum Transformers version requirement in run_translation.py
    Update the check_min_version call to enforce installation of Transformers version
    4.38.0.dev0 or later. This is important for compatibility with the latest features
    and fixes required by the translation script.

--- deepspeed/train_on_one_gpu-homework.sh
+++ deepspeed/train_on_one_gpu-homework.sh
@@ -1,11 +1,13 @@
 # DeepSpeed ZeRO-2 模式单 GPU 训练翻译模型（T5-Small）
 deepspeed --num_gpus=1 translation/run_translation.py \
 --deepspeed config/ds_config_zero2.json \
---model_name_or_path t5-small --per_device_train_batch_size 10 \
+--model_name_or_path t5-small --per_device_train_batch_size 40 \
 --output_dir models/t5-small-deepspeed-round1 --overwrite_output_dir --fp16 \
---do_train --num_train_epochs 2 \
+--do_train --num_train_epochs 1 \
 --dataset_name wmt16 --dataset_config "ro-en" \
---source_lang en --target_lang ro
+--source_lang en --target_lang ro \
+--source_prefix "translate English to Romanian: "
+
 
 # DeepSpeed ZeRO-2 模式单 GPU 训练翻译模型（T5-Large）
 deepspeed --num_gpus=1 translation/run_translation.py \
@@ -13,24 +15,25 @@ deepspeed --num_gpus=1 translation/run_translation.py \
 --model_name_or_path t5-large \
 --per_device_train_batch_size 4 \
 --per_device_eval_batch_size 4 \
---output_dir output_dir --overwrite_output_dir \
+--output_dir models/t5-large-deepspeed-round1 --overwrite_output_dir \
 --do_train \
 --do_eval \
 --max_train_samples 500 --num_train_epochs 1 \
 --dataset_name wmt16 --dataset_config "ro-en" \
---source_lang en --target_lang ro
+--source_lang en --target_lang ro \
+--source_prefix "translate English to Romanian: "
 
 
 
-# DeepSpeed ZeRO-3 模式单 GPU 训练翻译模型（T5-Large）
+# DeepSpeed ZeRO-3 模式单 GPU 训练翻译模型（T5-3b）
 deepspeed --num_gpus=1 translation/run_translation.py \
 --deepspeed config/ds_config_zero3.json \
 --model_name_or_path t5-3b --per_device_train_batch_size 1 \
---output_dir output_dir --overwrite_output_dir --fp16 \
+--output_dir models/t5-3b-deepspeed-round1 --overwrite_output_dir --fp16 \
 --do_train --max_train_samples 500 --num_train_epochs 1 \
 --dataset_name wmt16 --dataset_config "ro-en" \
---source_lang en --target_lang ro
-
+--source_lang en --target_lang ro \
+--source_prefix "translate English to Romanian: "
 
 
 # 直接使用 Python 命令启动 ZeRO-2 模式单 GPU 训练翻译模型（T5-Small）
